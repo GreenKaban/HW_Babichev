34c2a334cfc3edaa3becceb9ca74cddf2428d88c
greenkaban <kochetkov@ispras.ru>
2022-05-12 21:55:36 +0300

    add BULLY

--- contextes.h
+++ contextes.h
@@ -1,4 +1,3 @@
-
 struct context_common_s {
 };
 
@@ -6,11 +5,25 @@ struct context_common_s context_common;
 
 // ATTN context
 struct context_attn_s {
-  int  ready;
-  context_attn_s() {
-     ready = 0;
-  }
+    int  ready;
+    context_attn_s() {
+        ready = 0;
+    }
 };
 
 struct context_attn_s context_attn;
 
+struct context_bully_s {
+    int coord_id;
+    int start_time;
+    bool is_started;
+    bool got_alive_message;
+    context_bully_s() {
+        coord_id = -1;
+        start_time = -1;
+        is_started = false;
+        got_alive_message = false;
+    }
+};
+
+struct context_bully_s context_bully;
